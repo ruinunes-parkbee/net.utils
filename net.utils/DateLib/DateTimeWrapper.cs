00d5623a0641d356f16dd6fd459e9e4919dfa1e0
Rui Nunes <r.nunes@parkbee.com>
2018-01-25 13:51:01 +0100

    Add datetime lib

---
+++ net.utils/DateLib/DateTimeWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+using NodaTime;
+using NodaTime.TimeZones;
+
+namespace DateLib
+{
+    public interface IDateTimeWrapper
+    {
+        DateTime GetCountryTime(string countryCode);
+        DateTime GetCurrentUtcTime();
+        string GetUtcDateAsString();
+    }
+
+    public class DateTimeWrapper : IDateTimeWrapper
+    {
+        private const string EUROPEAN_DATE_FORMAT = "dd/MM/yyyy";
+
+        public DateTime GetCountryTime(string countryCode)
+        {
+            var utcDate = GetCurrentUtcTime();
+
+            var countryTimeZone = TzdbDateTimeZoneSource.Default.ZoneLocations?.FirstOrDefault(x => x.CountryCode == countryCode.ToUpper());
+            if (countryTimeZone != null)
+            {
+                var zone = DateTimeZoneProviders.Tzdb[countryTimeZone.ZoneId];
+                var offset = zone.GetUtcOffset(Instant.FromDateTimeUtc(utcDate));
+
+                return utcDate.AddSeconds(offset.Seconds);
+            }
+
+            return utcDate;
+        }
+
+        public DateTime GetCurrentUtcTime()
+        {
+            return DateTime.UtcNow;
+        }
+
+        public string GetUtcDateAsString()
+        {
+            return DateTime.UtcNow.ToString(EUROPEAN_DATE_FORMAT);
+        }
+    }
+}
